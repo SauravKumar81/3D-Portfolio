ca9a77a26b5aea9c91ed026a867bc95d7ee6bae2
Saurav Kumar <Sauravyt@gmail.com>
2025-07-16 15:33:59 +0530

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
-import React from "react";
-import Navbar from "./sections/navbar";
-import Hero from "./sections/Hero";
-import About from "./sections/About";
-import Projects from "./sections/Projects";
-import Experiences from "./sections/Experiences";
-import Testimonial from "./sections/Testimonial";
-import Contact from "./sections/Contact";
-import Footer from './sections/Footer';
-
+import React from 'react'
+import Navbar from './sections/Navbar'
+import Hero from './sections/Hero'
+import About from './sections/About'
 const App = () => {
   return (
-    <div className="container mx-auto max-w-7xl">
-      <Navbar />
-      <Hero />
-      <About />
-      <Projects />
-      <Experiences />
-      <Testimonial />
-      <Contact />
-      <Footer/>
-    </div>
-  );
-};
+    <div className='container mx-auto max-w-7xl'>
+    <Navbar />
+    <Hero/>
+    <About/>
 
-export default App;
+    
+    
+    
+    </div>
+  )
+}
+
+export default App
--- src/components/Globe.jsx
+++ src/components/Globe.jsx
@@ -120,3 +120,4 @@ export function Globe({ className, config = GLOBE_CONFIG }) {
     </div>
   );
 }
+export default Globe;
